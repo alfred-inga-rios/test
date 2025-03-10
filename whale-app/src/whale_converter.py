e3b338ee56b0f22c187101169c33a0174eda8514
a-ingarios-zurich <alfred.ingarios@zurich.com>
2025-03-10 10:52:37 +0100

    projects

--- whale-app/src/whale_converter.py
+++
@@ -1,6 +0,0 @@
-class WhaleConverter:
-    def __init__(self):
-        self.vowels = set('aeiouAEIOU')
-
-    def convert(self, text: str) -> str:
-        return ''.join(char for char in text if char not in self.vowels)
